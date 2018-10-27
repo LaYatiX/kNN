92049ea4d325b2407d92d15f224e409f432902ed
LaYatiX <gpiwosz@gmail.com>
2018-10-27 13:39:38 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+class EuklidesToSpokoGosc:
+    k = 0
+    data = []
+
+    def __init__(self, k, data):
+        self.k = k
+        self.data = data
+
+    def predict(self, data):
+        return []
+
+    def score(self, data, labels):
+        return 0
+
+
